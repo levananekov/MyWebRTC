312aaeb63f40ff82210399596208be3a75f40447
steep_11 <steep_1183@gmail.com>
2019-08-09 14:00:08 +0500

    createOffer

---
+++ app/src/main/java/levananenkov/myapplication/mywebrtc/TurnServer.java
@@ -0,0 +1,10 @@
+package levananenkov.myapplication.mywebrtc;
+
+import retrofit2.Call;
+import retrofit2.http.Header;
+import retrofit2.http.PUT;
+
+public class TurnServer {
+//    @PUT("/_turn/<xyrsys_channel>")
+//    Call<TurnServerPojo> getIceCandidates(@Header("Authorization") String authkey);
+}
---
+++ app/src/main/java/levananenkov/myapplication/mywebrtc/Utils.java
@@ -0,0 +1,29 @@
+package levananenkov.myapplication.mywebrtc;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Utils {
+
+    static Utils instance;
+    public static final String API_ENDPOINT = "https://global.xirsys.net";
+
+    public static Utils getInstance() {
+        if (instance == null) {
+            instance = new Utils();
+        }
+        return instance;
+    }
+
+//    private Retrofit retrofitInstance;
+//
+//    TurnServer getRetrofitInstance() {
+//        if (retrofitInstance == null) {
+//            retrofitInstance = new Retrofit.Builder()
+//                    .baseUrl(API_ENDPOINT)
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .build();
+//        }
+//        return retrofitInstance.create(TurnServer.class);
+//    }
+}
